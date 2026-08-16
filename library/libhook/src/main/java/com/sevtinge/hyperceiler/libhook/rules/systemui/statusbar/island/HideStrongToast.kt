a5434b0eca1721cac826dca067e4cbd528837609
GreenTeodoro839 <77104800+GreenTeodoro839@users.noreply.github.com>
2026-08-16 11:43:22 +0800

    fix: 适配 HyperOS 4 灵动舞台隐藏

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemui/statusbar/island/HideStrongToast.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemui/statusbar/island/HideStrongToast.kt
@@ -18,15 +18,32 @@
 */
 package com.sevtinge.hyperceiler.libhook.rules.systemui.statusbar.island
 
+import android.os.Bundle
 import android.widget.FrameLayout
 import com.sevtinge.hyperceiler.libhook.base.BaseHook
+import com.sevtinge.hyperceiler.libhook.utils.api.DeviceHelper.System.isMoreHyperOSVersion
 import com.sevtinge.hyperceiler.libhook.utils.hookapi.LazyClass.NewStrongToast
 import io.github.lingqiqi5211.ezhooktool.core.findMethod
+import io.github.lingqiqi5211.ezhooktool.core.loadClass
 import io.github.lingqiqi5211.ezhooktool.xposed.dsl.createAfterHook
+import io.github.lingqiqi5211.ezhooktool.xposed.dsl.createBeforeHook
 
 
 object HideStrongToast : BaseHook() {
     override fun init() {
+        if (isMoreHyperOSVersion(4f)) {
+            loadClass("com.android.systemui.devicenotification.listener.DeviceNotificationListenerImpl")
+                .findMethod {
+                    name("setStatus")
+                    parameterTypes(Int::class.java, String::class.java, Bundle::class.java)
+                }.createBeforeHook { param ->
+                    if (param.args[1] == "strong_toast_action") {
+                        param.result = null
+                    }
+                }
+            return
+        }
+
         NewStrongToast!!.findMethod { name("onAttachedToWindow") }.createAfterHook {
             val strongToastLayout = it.thisObject as FrameLayout
             strongToastLayout.viewTreeObserver.addOnPreDrawListener {
